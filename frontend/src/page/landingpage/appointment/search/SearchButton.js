045b8d77a52f37c57601365a224bd6ac5439b070
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-02-06 22:53:30 +0100

    update 6.2

--- frontend/src/page/landingpage/appointment/search/SearchButton.js
+++ frontend/src/page/landingpage/appointment/search/SearchButton.js
@@ -3,7 +3,9 @@ import React from 'react'
 const SearchButton = () => {
     return (
         <div>
+            <Link to="/result-page">
             <button className='search-btn search-btn-p'>Search</button>
+        </Link>
         </div>
     )
 }
